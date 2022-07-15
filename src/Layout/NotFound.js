56edb6f12df00d3a1c8524b2e928754713f9794c
Ibrahima Diallo <Ibrahima1d@gmail.com>
2022-07-15 16:09:33 -0400

    Update commits

---
+++ src/Layout/NotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NotFound() {
+
+  return (
+    <div className="NotFound">
+      <h1>Not Found</h1>
+      <p>Sorry, the requested URL does not exist.</p>
+    </div>
+  );
+}
+
+export default NotFound;
